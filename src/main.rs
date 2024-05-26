6b7ac1d0c97c5446e91b81f1267bb6c3270c8ee7
RasmusTufvesson <lars@bizzbook.com>
2024-05-26 18:01:23 +0200

    copy code from example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use crate::bot::run;
+
+mod bot;
+
+#[tokio::main]
+async fn main() {
+    let token = include_str!("../TOKEN");
+    run(token).await;
 }
